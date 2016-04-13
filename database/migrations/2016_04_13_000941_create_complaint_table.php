bc9e140721d73e980203e94e350c574903226520
Marco Oliveira <marcoaacoliveira@gmail.com>
2016-04-12 21:41:45 -0300

    Criado migration para a tabela complaint.

---
+++ database/migrations/2016_04_13_000941_create_complaint_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComplaintTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaint', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('state');
+            $table->string('city');
+            $table->decimal('value');
+            $table->string('company');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('complaint');
+    }
+}
