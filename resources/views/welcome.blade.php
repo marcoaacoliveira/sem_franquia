cec845ceaa7a72b20bb33176340103c09ff3b284
Marco Oliveira <marcoaacoliveira@gmail.com>
2016-04-15 23:14:56 -0300

    Adicionado layout básico para o site.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,72 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sem Franquia</title>
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:50">
+    <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
+</head>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="jumbotron">
+                <h1 class="text-center">Sem Franquia</h1>
+                <p class="text-center">Está insatisfeito com a possibilidade de franquias na banda larga brasileira?
+                    Deixe sua reclamação aqui e ajude o somátorio total de prejuízo para empresas caso todos nós
+                    saissemos de nossos planos mensais.</p>
             </div>
         </div>
-    </body>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center big-size">R$ 2.300,00</h1>
+            <p class="text-uppercase text-center"><strong>Esse é o total da receita mensal que as empresas
+                    perderiam,caso todos os usuários que deixaram sua insatisfação aqui desistissem de seus
+                    planos</strong></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <form class="bootstrap-form-with-validation">
+                <h2 class="text-center">Quero registrar minha insatisfação</h2>
+                <div class="form-group">
+                    <label class="control-label" for="text-input">Estado </label>
+                    <select class="form-control">
+                        <option value="1">Bahia</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="password-input">Cidade </label>
+                    <select class="form-control">
+                        <option value="1">Salvador</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="email-input">Empresa </label>
+                    <select class="form-control">
+                        <option value="1">GVT</option>
+                        <option value="13">Velox</option>
+                        <option value="14">NET</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="textarea-input">Valor mensal do plano de internet</label>
+                    <input class="form-control" type="text" name="plan" required="" id="plan">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block" type="submit">ENVIAR</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+</body>
+
 </html>
